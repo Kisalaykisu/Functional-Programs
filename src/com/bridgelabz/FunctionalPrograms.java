525e0d16b6bf491fed92e1c4a7f13ef731390c43
Kisalay <kisalaykisu@gmail.com>
2022-06-20 12:55:01 +0530

    4th commit

--- src/com/bridgelabz/FunctionalPrograms.java
+++ src/com/bridgelabz/FunctionalPrograms.java
@@ -1,6 +1,9 @@
-/* Q-3. Write a program Distance.java that takes two integer command-line arguments x
-and y and prints the Euclidean distance from the point (x, y) to the origin (0, 0). The
-formulae to calculate distance = sqrt(x*x + y*y). Use Math.power function
+/* Q-4). Write a program Quadratic.java to find the roots of the equation a*x*x + b*x + c.
+Since the equation is x*x, hence there are 2 roots. The 2 roots of the equation
+can be found using a formula (Note: Take a, b and c as input values)
+delta = b*b - 4*a*c
+Root 1 of x = (-b + sqrt(delta))/(2*a)
+Root 2 of x = (-b - sqrt(delta))/(2*a)
 
 */
 
@@ -12,14 +15,17 @@ import java.util.Scanner; // Import Scanner
 public class FunctionalPrograms {
     public static void main(String[] args){   //Entry Point of Program
 
-        Scanner scanner = new Scanner(System.in);   // Make Scanner Obj
-        System.out.println("Enter the co-ordinates x,y: ");
-        int x = scanner.nextInt();  // Input Int
-        int y = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);  //mAKE Scanner obj
+        System.out.println("Enter the coefficients a,b,c of the quadratic equation : ");
+        int a = scanner.nextInt();  // Input Int
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-        double distance = Math.sqrt(x*x+y*y);  //Mathematical Formula
-        System.out.println("The Euclidean Distance of the point from origin is = "+ distance);
+        double delta = b*b - 4*a*c;
+        double root1 = (-b + Math.sqrt(delta))/(2*a);  //Quadratic Equation
+        double root2 = (-b - Math.sqrt(delta))/(2*a);
 
+        System.out.println("The roots are: " +root1+ " and "+ root2);
 
     }
 
