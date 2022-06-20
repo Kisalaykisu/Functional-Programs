63fcb5c96a8c08040b76e33790025ca87f865abc
Kisalay <kisalaykisu@gmail.com>
2022-06-20 13:22:50 +0530

    5th commit

--- src/com/bridgelabz/FunctionalPrograms.java
+++ src/com/bridgelabz/FunctionalPrograms.java
@@ -1,9 +1,10 @@
-/* Q-4). Write a program Quadratic.java to find the roots of the equation a*x*x + b*x + c.
-Since the equation is x*x, hence there are 2 roots. The 2 roots of the equation
-can be found using a formula (Note: Take a, b and c as input values)
-delta = b*b - 4*a*c
-Root 1 of x = (-b + sqrt(delta))/(2*a)
-Root 2 of x = (-b - sqrt(delta))/(2*a)
+/* Q-5. Write a program WindChill.java that takes two double command-line arguments t
+and v and prints the wind chill. Use Math.pow(a, b) to compute ab. Given the
+temperature t (in Fahrenheit) and the wind speed v (in miles per hour), the
+National Weather Service defines the effective temperature (the wind chill) to be:
+
+Note: the formula is not valid if t is larger than 50 in absolute value or if v is larger
+than 120 or less than 3 (you may assume that the values you get are in that range).
 
 */
 
@@ -13,21 +14,33 @@ package com.bridgelabz;  //package
 import java.util.Scanner; // Import Scanner
 
 public class FunctionalPrograms {
-    public static void main(String[] args){   //Entry Point of Program
+    public static void main(String[] args) {   //Entry Point of Program
 
-        Scanner scanner = new Scanner(System.in);  //mAKE Scanner obj
-        System.out.println("Enter the coefficients a,b,c of the quadratic equation : ");
-        int a = scanner.nextInt();  // Input Int
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+            int[] arr=UserInput();  //TAke input choice
+            int t = arr[0]; //Array initializer
+            int v = arr[1];
 
-        double delta = b*b - 4*a*c;
-        double root1 = (-b + Math.sqrt(delta))/(2*a);  //Quadratic Equation
-        double root2 = (-b - Math.sqrt(delta))/(2*a);
+            while(t>50 || v>120 || v<3){
 
-        System.out.println("The roots are: " +root1+ " and "+ root2);
+                arr=UserInput();
+                t = arr[0];
+                v = arr[1];
+            }
+            double w = 35.74 + 0.6215*t + (0.4275*t+35.75)*Math.pow(v,0.16);
+            System.out.println("The WindChill is : " + w);
+        }
+        public static int[] UserInput(){
 
-    }
+            Scanner scanner = new Scanner(System.in);  //Make Scanner obj
+            System.out.println("Enter temperature in F and wind speed in mph : ");
+            int t = scanner.nextInt();  //Input Int
+            int v = scanner.nextInt();
+            int[] arr = new int[2];   // array declaration and initialization
+            arr[0]=t;
+            arr[1]=v;
+            return arr;
+        }
 
 }
+
 
