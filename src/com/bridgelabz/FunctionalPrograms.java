0c102e53970a9c690a54a5f5d7dd7ab3f826e0e7
Kisalay <kisalaykisu@gmail.com>
2022-06-20 12:47:57 +0530

    3rd commit

--- src/com/bridgelabz/FunctionalPrograms.java
+++ src/com/bridgelabz/FunctionalPrograms.java
@@ -1,62 +1,26 @@
-/* Q-2. Sum of three Integer adds to ZERO
-a. Desc -> A program with cubic running time. Read in N integers and counts the
-number of triples that sum to exactly 0.
-b. I/P -> N number of integer, and N integer input array
-c. Logic -> Find distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
-d. O/P -> One Output is number of distinct triplets as well as the second output is to
-print the distinct triplets.
+/* Q-3. Write a program Distance.java that takes two integer command-line arguments x
+and y and prints the Euclidean distance from the point (x, y) to the origin (0, 0). The
+formulae to calculate distance = sqrt(x*x + y*y). Use Math.power function
 
 */
 
 
 package com.bridgelabz;  //package
 
-import java.util.Arrays;  //Import arrays
 import java.util.Scanner; // Import Scanner
 
 public class FunctionalPrograms {
-    static void findTriplets(int[] arr, int n)  //Initialize with function with 2 parameters.
-    {
-        int numberOfDistinctTriples = 0;
-        boolean found = false;     // Initialize condition
-        for (int i=0; i<n-2; i++)     // Nested for loop
-        {
-            for (int j=i+1; j<n-1; j++)
-            {
-                for (int k=j+1; k<n; k++)
-                {
-                    if (arr[i]+arr[j]+arr[k] == 0)
-                    {
-                        System.out.print(arr[i]+" ");  // 1st element
-                        System.out.print(arr[j]+" ");  // 2nd element
-                        System.out.print(arr[k]+" "+"\n"); //3rd element
-                        numberOfDistinctTriples++;
+    public static void main(String[] args){   //Entry Point of Program
 
-                        found = true;
-                    }
-                }
-            }
-        }
-        if (found == false) {
-            System.out.println(" not exist ");
-        }
-        System.out.println("number of distinct triples = "+numberOfDistinctTriples);
-    }
+        Scanner scanner = new Scanner(System.in);   // Make Scanner Obj
+        System.out.println("Enter the co-ordinates x,y: ");
+        int x = scanner.nextInt();  // Input Int
+        int y = scanner.nextInt();
 
-    public static void main(String[] args) {
+        double distance = Math.sqrt(x*x+y*y);  //Mathematical Formula
+        System.out.println("The Euclidean Distance of the point from origin is = "+ distance);
 
-        Scanner scanner = new Scanner(System.in);  // Make Scanner Obj
-        System.out.println("Enter no of integers in the array: ");
-        int N = scanner.nextInt();
-        int[] array = new int[N];
 
-        System.out.println("Enter the integer values in the array: ");
-        for (int i=0; i<N;i++){
-            array[i]=scanner.nextInt();
-        }
-        System.out.println(Arrays.toString(array)); //TypeCasting
-
-        findTriplets(array, N);  //Calling Function with 2 Parameters
     }
 
 }
