a72388cecde3d4497323ded168ab2fe9bd0e004a
Kisalay <kisalaykisu@gmail.com>
2022-06-20 12:12:56 +0530

    1st commit

---
+++ src/com/bridgelabz/FunctionalPrograms.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class FunctionalPrograms {
+    public static void main(String[] args){
+
+        System.out.println("Welcome to the Functional Programming Playground :");
+    }
+}
